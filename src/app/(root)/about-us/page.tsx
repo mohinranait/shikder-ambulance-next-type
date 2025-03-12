0aa2ab6bce21d89b6e629b58349de8ee90203c4f
Mohin Rana <ebrahimit49@gmail.com>
2025-03-12 09:05:50 +0600

    Update about page

--- src/app/(root)/about-us/page.tsx
+++ src/app/(root)/about-us/page.tsx
@@ -1,4 +1,15 @@
 import AboutSection from "@/components/pages/home/AboutSection";
+import { Button } from "@nextui-org/button";
+import {
+  Ambulance,
+  AmbulanceIcon,
+  Car,
+  CarFront,
+  Cross,
+  User,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 import React from "react";
 
@@ -6,7 +17,14 @@ export const metadata = {
   title: "About Us: Shikder Ambulance",
   description:
     "About Us: Shikder Ambulance is the best Ambulance Company in Bangladesh. We provide the different types of Ambulances in the all districts of Bangladesh. 24 Hours Ambulance Support is Available.",
-  keywords: ["About Us", "ambulance service", "emergency ambulance", "Dhaka ambulance", "best ambulance", "Shikder Ambulance"],
+  keywords: [
+    "About Us",
+    "ambulance service",
+    "emergency ambulance",
+    "Dhaka ambulance",
+    "best ambulance",
+    "Shikder Ambulance",
+  ],
   openGraph: {
     title: "Best Ambulance Service in Dhaka | 24/7 Support",
     description: "We provide 24/7 emergency ambulance service in Dhaka.",
@@ -24,10 +42,176 @@ export const metadata = {
   },
 };
 
+const ambulanceTypes = [
+  {
+    _id: "1",
+    icon: <Car className="text-primary" size={30} />,
+    title: "Ac ambulance service",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae illum?",
+    url: "/",
+  },
+  {
+    _id: "2",
+    icon: <Ambulance className="text-primary" size={30} />,
+    title: "Ac ambulance service",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae illum?",
+    url: "/",
+  },
+  {
+    _id: "3",
+    icon: <CarFront className="text-primary" size={30} />,
+    title: "Ac ambulance service",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae illum?",
+    url: "/",
+  },
+  {
+    _id: "4",
+    icon: <Cross className="text-primary" size={30} />,
+    title: "Ac ambulance service",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae illum?",
+    url: "/",
+  },
+  {
+    _id: "5",
+    icon: <Cross className="text-primary" size={30} />,
+    title: "Ac ambulance service",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae illum?",
+    url: "/",
+  },
+  {
+    _id: "6",
+    icon: <Cross className="text-primary" size={30} />,
+    title: "Ac ambulance service",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae illum?",
+    url: "/",
+  },
+];
+
 const AboutPage = async () => {
   return (
     <>
-      <AboutSection />
+      <section className="bg-primary">
+        <div className="container py-16 ">
+          <h1 className="text-white mb-2 text-4xl font-semibold text-center">
+            About US: Shikder Ambulance Service
+          </h1>
+          <p className="text-center text-slate-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            autem earum laudantium qui accusantium sit.
+          </p>
+          <ul className="flex items-center gap-2 pt-2 justify-center">
+            <li>
+              <Link href="/" className="font-medium text-base text-slate-200">
+                Home
+              </Link>
+            </li>
+            <li className="font-medium text-base text-slate-200">/</li>
+            <li className="font-medium text-base text-slate-200">About Us</li>
+          </ul>
+        </div>
+      </section>
+      <section className="py-10">
+        <div className="container">
+          <h2 className="text-center text-xl font-bold">
+            About Shikder ambulance
+          </h2>
+          <p className="text-center ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+            doloribus.
+          </p>
+          <div className="grid md:grid-cols-2 mt-5 gap-5">
+            <div>
+              <h3 className="text-base font-bold">About us</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
+                voluptatum vitae ratione quaerat beatae amet.
+              </p>
+              <ul className="list-disc mt-2 pl-5">
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+            </div>
+            <div>
+              <Image
+                src={"/images/ambulnace.jpg"}
+                width={600}
+                height={400}
+                alt="Ac Ambulance Service"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container ">
+          <h2 className="text-2xl font-bold text-center">
+            Type of your service
+          </h2>
+          <div className="grid md:grid-cols-2 mt-3 lg:grid-cols-3 gap-5">
+            {ambulanceTypes?.map((item, index) => (
+              <div
+                key={index}
+                className="border space-y-3 border-slate-200 rounded p-5"
+              >
+                <div className="w-14 h-14 flex items-center justify-center mx-auto rounded-full bg-primary-mute">
+                  {item.icon}
+                </div>
+                <h3 className="text-base text-center font-normal text-slate-950">
+                  {item?.title}
+                </h3>
+                <p className="mb-4 text-sm text-center text-slate-700">
+                  {item?.content}
+                </p>
+                <div className="flex justify-center">
+                  <Link
+                    href={`${item?.url}`}
+                    className="py-1 inline-block px-2 bg-primary text-white text-xs rounded"
+                  >
+                    Learn more...
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-10">
+        <div className="container">
+          <h2 className="text-center text-xl font-bold">
+            About Shikder ambulance
+          </h2>
+          <p className="text-center ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+            doloribus.
+          </p>
+          <div className=" mt-5">
+            <div>
+              <h3 className="text-base font-bold">About us</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
+                voluptatum vitae ratione quaerat beatae amet. Lorem ipsum dolor,
+                sit amet consectetur adipisicing elit. Nihil voluptatum vitae
+                ratione quaerat beatae amet.
+              </p>
+              <ul className="list-disc mt-2 pl-5">
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/components/pages/home/Faq.tsx
+++ src/components/pages/home/Faq.tsx
@@ -7,34 +7,39 @@ const Faq = () => {
 
   const faqs = [
     {
-      _id: '1',
+      _id: "1",
       title: "What is the Shikder Ambulance service?",
-      content: 'Shikder Ambulance is the largest and trusted Ambulance provider company in Bangladesh. We provide different types of Ambulances. When you need Ambulance support then Contact Us.'
+      content:
+        "Shikder Ambulance is the largest and trusted Ambulance provider company in Bangladesh. We provide different types of Ambulances. When you need Ambulance support then Contact Us.",
     },
     {
-      _id: '2',
+      _id: "2",
       title: "What is an ambulance service, and when should I call one?",
-      content: 'Ambulance services consist of specialized vehicles outfitted to deliver medical care and transport individuals during emergencies. It is crucial to dial an ambulance in scenarios involving serious injuries, medical crises, or when someone is life is in jeopardy. '
+      content:
+        "Ambulance services consist of specialized vehicles outfitted to deliver medical care and transport individuals during emergencies. It is crucial to dial an ambulance in scenarios involving serious injuries, medical crises, or when someone is life is in jeopardy. ",
     },
     {
-      _id: '3',
-      title: "Can I request a specific hospital for the ambulance to take me to?",
-      content: ' Yes,  you may have the option to designate a particular hospital. Nevertheless, the ambulance crew will give precedence to the closest suitable medical facility, considering the patientis condition.'
+      _id: "3",
+      title:
+        "Can I request a specific hospital for the ambulance to take me to?",
+      content:
+        " Yes,  you may have the option to designate a particular hospital. Nevertheless, the ambulance crew will give precedence to the closest suitable medical facility, considering the patientis condition.",
     },
 
     {
-      _id: '4',
+      _id: "4",
       title: "How often is the ambulance service provided?",
-      content: '24/7 Hours Ambulance is Available in Bangladesh. We are always ready to provide emergency Ambulance provide to you.'
+      content:
+        "24/7 Hours Ambulance is Available in Bangladesh. We are always ready to provide emergency Ambulance provide to you.",
     },
 
     {
-      _id: '5',
+      _id: "5",
       title: "How can I book the services of Shikder Ambulance?",
-      content: ' You can book An Ambulance at Shikder Ambulance through our hassle-free appointment system. You can contact us by Phone, or Online Calling by Whatsapp, and our team is is available 24/7 to assist you. If you need emergency Ambulances then contact Us: 017100-60020'
+      content:
+        " You can book An Ambulance at Shikder Ambulance through our hassle-free appointment system. You can contact us by Phone, or Online Calling by Whatsapp, and our team is is available 24/7 to assist you. If you need emergency Ambulances then contact Us: 017100-60020",
     },
-
-  ]
+  ];
   return (
     <section
       className="bg-no-repeat bg-cover bg-center"
@@ -52,7 +57,9 @@ const Faq = () => {
               Frequently Asked Questions (FAQs)
             </h2>
             <p className="text-center text-white">
-              When people interact with ambulance services, whether in an emergency or seeking information, they commonly have several key questions.
+              When people interact with ambulance services, whether in an
+              emergency or seeking information, they commonly have several key
+              questions.
             </p>
           </div>
         </div>
@@ -61,19 +68,19 @@ const Faq = () => {
             variant="splitted"
             className="flex flex-col gap-4 "
             showDivider={false}
-            defaultExpandedKeys={["1"]}
+            defaultExpandedKeys={["0"]}
           >
-            {
-              faqs?.map((item, index) => <AccordionItem
+            {faqs?.map((item, index) => (
+              <AccordionItem
                 key={`${index}`}
                 aria-label={`Accordion ${index}`}
                 title={<p className="text-white text-left">{item?.title}</p>}
                 className="bg-slate-100 bg-opacity-15 p-1 pl-4 rounded-lg  text-white"
-              // indicator={<FaBars />}
+                // indicator={<FaBars />}
               >
                 {item?.content}
-              </AccordionItem>)
-            }
+              </AccordionItem>
+            ))}
           </Accordion>
         </div>
       </div>
